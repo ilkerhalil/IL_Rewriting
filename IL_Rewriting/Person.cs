f976c0da923e2f0ae7dcdb004cf1cec72c8a336d
Ilker Halil Turer <ilkerhalil@gmail.com>
2015-08-24 01:06:02 +0300

    First Commit

---
+++ IL_Rewriting/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace IL_Rewriting
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public int PerKod { get; set; }
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
---
+++ IL_Rewriting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+
+namespace IL_Rewriting
+{
+    class Program
+    {
+        static void Main()
+        {
+            var assemblyDefinition=   AssemblyDefinition.ReadAssembly("IL_Rewriting.exe");
+            var type = assemblyDefinition.Modules.Single(w => w.Types.Single(s => s.Name == "Person") != null).Types.Single(s=> s.Name=="Person");
+            
+        }
+    }
+}
